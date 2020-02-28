848eaee8af011fc9d3728b6f252b8e5818ee8f9e
mikedev <mik3dev@gmail.com>
2020-02-28 16:11:01 +0100

    fixes test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from dataset import sample_batch, pull_data_omniglot
 from fire import Fire
 import torch
 import gc
+from tqdm import tqdm
 
 
 def main(dataset='omniglot',
@@ -21,14 +22,12 @@ def main(dataset='omniglot',
     else:
         embedding_network = build_embedding_network_miniimagenet().to(device)
         snail = build_snail_miniimagenet(n, t).to(device)
-    # embedding_network.load_state_dict(torch.load(embedding_path))
-    #todo fix import of snail weights
-    # snail.load_state_dict(torch.load(model_path))
+    embedding_network.load_state_dict(torch.load(embedding_path))
+    snail.load_state_dict(torch.load(model_path))
     embedding_network = embedding_network.eval()
     snail = snail.eval()
     embedding_network.requires_grad_(False)
     snail.requires_grad_(False)
-
     with open(test_classes_path) as f:
         test_classes = f.read()
     test_classes = list(map(Path, test_classes.split(', ')))
@@ -36,8 +35,8 @@ def main(dataset='omniglot',
     loss_values = torch.zeros(n_sample)
     acc_values = torch.zeros(n_sample)
     ohe_matrix = torch.eye(n)
-    for i in range(n_sample):
-        X, y, y_last = sample_batch(batch_size, test_classes, t, n, k, ohe_matrix)
+    for i in tqdm(range(n_sample)):
+        X, y, y_last = sample_batch(batch_size, test_classes, t, n, k, random_rotation=False, ohe_matrix=ohe_matrix)
         yhat = predict(embedding_network, snail, X, y)
         p_yhat_last: torch.Tensor = yhat[:, :, -1]
         loss_value = loss(p_yhat_last, y_last)
@@ -46,14 +45,14 @@ def main(dataset='omniglot',
         loss_values[i] = loss_value
         acc_values[i] = acc
         gc.collect()
-    print('\n', '='*100, '\n', sep='', end='')
-    print('loss values:', loss_values)
-    print('avg loss:', loss_values.mean())
-    print('\n', '='*100, '\n', sep='', end='')
-    print('acc values:', acc_values)
-    print('avg acc:', acc_values.mean())
-    print('\n', '='*100, '\n', sep='', end='')
+    print('\n', '='*80, '\n', sep='', end='')
+    print('avg loss:', loss_values.mean().item())
+    print('std loss:', loss_values.std(unbiased=True).item())
+    print('\n', '='*80, '\n', sep='', end='')
+    print('avg acc:', acc_values.mean().item())
+    print('std acc:', acc_values.std(unbiased=True).item())
+    print('\n', '='*80, '\n', sep='', end='')
 
 
 if __name__ == '__main__':
-    main()
+    Fire(main)
