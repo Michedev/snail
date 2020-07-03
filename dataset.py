3b5f2b2ac6a4559523f144b130550a38c44e19cc
mikedev <mik3dev@gmail.com>
2020-07-03 02:06:30 +0200

    change int tensor to long match the other one

--- dataset.py
+++ dataset.py
@@ -74,7 +74,7 @@ def fit_last_image(X, classes, image_names_batch, n, rotations):
 
 def shuffle_data(X, y):
     i = torch.randperm(len(X)-1)
-    i_last = torch.IntTensor([len(X)-1])
+    i_last = torch.LongTensor([len(X)-1])
     i = torch.cat([i, i_last])
     X = X[i]
     y = y[i]
