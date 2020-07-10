bbf12a5b6ecfa0c32e24454540c1503baeb81d16
Mikedev <mdv1994@gmail.com>
2020-07-10 02:28:19 +0200

    add reflectionpad modules

--- modules.py
+++ modules.py
@@ -1,4 +1,5 @@
 from torch.nn import *
+from torch.nn import ReflectionPad1d
 import torch
 from math import log2, ceil, sqrt, floor
 
@@ -10,8 +11,16 @@ class DenseBlock(Module):
         self.dilatation = dilation
         self.in_filters = in_filters
         self.out_filters = out_filters
-        self.causal_conv1 = Conv1d(in_filters, out_filters, kernel_size=2, dilation=dilation)
-        self.causal_conv2 = Conv1d(in_filters, out_filters, kernel_size=2, dilation=dilation)
+        self.causal_conv1 = Sequential(
+            ReflectionPad1d((0, dilation)),
+            Conv1d(in_filters, out_filters, kernel_size=2,
+                                   dilation=dilation)
+        )
+        self.causal_conv2 = Sequential(
+            ReflectionPad1d((0, dilation)),
+            Conv1d(in_filters, out_filters, kernel_size=2,
+                                   dilation=dilation)
+        )        
         self.tanh = Tanh()
         self.sigmoid = Sigmoid()
 
