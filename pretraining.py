b7a9cee8e0490951731bd817de2650b27d9b0450
Mikedev <mdv1994@gmail.com>
2020-07-14 23:51:56 +0200

    fixed errors pretraining

--- pretraining.py
+++ pretraining.py
@@ -3,7 +3,7 @@ from torch.nn import Sequential, Linear, BatchNorm1d, ReLU, LeakyReLU, Softmax, 
 from models import build_embedding_network_miniimagenet
 from torch.utils.data import DataLoader
 from collections import OrderedDict
-from ignite.metrics import Accuracy
+from ignite.metrics import Accuracy, Loss
 from ignite.engine import create_supervised_trainer, Events, create_supervised_evaluator
 from itertools import chain
 from paths import MINIIMAGENETFOLDER, WEIGHTSFOLDER
@@ -11,10 +11,11 @@ from PIL import Image
 from ignite.metrics import RunningAverage
 from torchvision import transforms
 from ignite.contrib.handlers.tqdm_logger import ProgressBar
+from fire import Fire
 
 
-TRAIN_MINIIMAGET = MINIIMAGENETFOLDER / 'train'
-
+TRAIN_MINIIMAGENET = MINIIMAGENETFOLDER / 'train'
+EMBEDDING_PATH = WEIGHTSFOLDER / 'embedding_miniimagenet.pth'
 
 class SupervisedMiniImagenet(torch.utils.data.Dataset):
     
@@ -22,7 +23,7 @@ class SupervisedMiniImagenet(torch.utils.data.Dataset):
         super().__init__()
         self.n = len(classes)
         self.dict_classes = {cl: i for i, cl in enumerate(classes)}
-        self.files = [[cl_file, cl] for cl_file in cl.files() for cl in classes]
+        self.files = [[cl_file, cl] for cl in classes for cl_file in cl.files()]
         self.preprocess_image = transforms.Compose([
                 transforms.Resize([84, 84]),
                 transforms.ToTensor(),
@@ -39,17 +40,19 @@ class SupervisedMiniImagenet(torch.utils.data.Dataset):
         img = self.preprocess_image(img)
         return img, y
 
-
         
 
 def build_model_pretraining(num_classes):
-    d = OrderedDict()
-    d['embedding_nn'] = build_embedding_network_miniimagenet()
-    d['classifier'] = Sequential(Linear(384, 100), BatchNorm1d(100), ReLU(), Linear(100, num_classes), Softmax(1))
-    return ModuleDict(d)
+    embedding_nn = build_embedding_network_miniimagenet()
+    classifier = Sequential(Linear(384, 100), BatchNorm1d(100), ReLU(), Linear(100, num_classes), Softmax(1))
+    model = Sequential()
+    model.add_module('embedding_nn', embedding_nn)
+    model.add_module('classifier', classifier)
+    return model
+
 
 def train_model(model, classes, device, epochs, batch_size):
-    opt = torch.optim.Adam(model.paramters())
+    opt = torch.optim.Adam(model.parameters())
     loss = CrossEntropyLoss()
     trainer = create_supervised_trainer(model, opt, loss, device=torch.device(device))
 
@@ -59,16 +62,20 @@ def train_model(model, classes, device, epochs, batch_size):
     train_data, test_data = torch.utils.data.random_split(dataset, [train_len, test_len])
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
-
     RunningAverage(output_transform=lambda x: x).attach(trainer, 'loss')
-    Accuracy().attach(trainer, 'accuracy')
     p = ProgressBar()
-    p.attach(trainer, ['loss', 'accuracy'])
+    p.attach(trainer, ['loss'])
 
     @trainer.on(Events.EPOCH_COMPLETED)
     def eval_test(engine):
-        metrics = dict(accuracy=Accuracy(), crossentropy=CrossEntropyLoss(reduction='mean'))
-        evaluator = create_supervised_evaluator(model, metrics)
+        print('Epoch', engine.state.epoch)
+        metrics = dict(accuracy=Accuracy(), crossentropy=Loss(CrossEntropyLoss()))
+        evaluator = create_supervised_evaluator(model, metrics, device=torch.device(device))
+        evaluator.run(train_loader, max_epochs=1)
+        acc = evaluator.state.metrics['accuracy']
+        print('Train accuracy', evaluator.state.metrics['accuracy'])
+        print('Train crossentropy', evaluator.state.metrics['crossentropy'])
+        evaluator = create_supervised_evaluator(model, metrics, device=torch.device(device))
         evaluator.run(test_loader, max_epochs=1)
         acc = evaluator.state.metrics['accuracy']
         print('Test accuracy', evaluator.state.metrics['accuracy'])
@@ -76,7 +83,20 @@ def train_model(model, classes, device, epochs, batch_size):
 
     @trainer.on(Events.EPOCH_COMPLETED)
     def save_embedding(engine):
-        torch.save(model['embedding_nn'], WEIGHTSFOLDER / 'embedding_miniimagenet.pth')
+        torch.save(model[0].state_dict(), WEIGHTSFOLDER / 'embedding_miniimagenet.pth')
 
-    trainer.run(train_data, max_epochs=epochs)
+    trainer.run(train_loader, max_epochs=epochs)
 
+
+
+def main(epochs, batch_size, device='cuda'):
+    train_classes = TRAIN_MINIIMAGENET.dirs()
+    model = build_model_pretraining(len(train_classes))
+    model = model.to(device)
+    if EMBEDDING_PATH.exists():
+        model.load_state_dict(torch.load(EMBEDDING_PATH, map_location=torch.device(device)))
+        print('loaded embedding from', EMBEDDING_PATH)
+    train_model(model, train_classes, device, epochs, batch_size)
+
+if __name__ == "__main__":
+    Fire(main)
